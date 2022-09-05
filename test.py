cd187305fc17a787c7b7e2e95a108fd9687743cb
Christopher Johnstone <meson800@gmail.com>
2022-09-05 12:48:21 -0400

    Updated submodule revision

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     print('Small test file:')
     fcs = _libfcs_ext.FCS('../libfcs/tests/fcs_files/attune_small.fcs')
     print(fcs.uncompensated)
+    del fcs
     print('Large test file:')
     fcs = _libfcs_ext.FCS('../libfcs/tests/fcs_files/attune.fcs')
     print(fcs.uncompensated)
